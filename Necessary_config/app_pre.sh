c44f3eeccc3b1b5d704202f69a95950bd83afa16
Luxury <111971103+luxuxl@users.noreply.github.com>
2024-09-11 12:59:58 +0800

    Add shell command files

---
+++ Necessary_config/app_pre.sh
@@ -0,0 +1,155 @@
+# ----------------------- Finder - View Options ----------------------
+# 修改桌面视图设置
+# 存在问题: 初始状态下 com.apple.finder 中没有 DesktopViewSettings
+# 解决方法一: 用 AppleScript 操作会自动生成, 同时可以直接 set 相关属性
+# 方法一的缺点: 1、需要获取 Finder 操作权限 2、aps 无法设置 gridSpacing
+# 缺点的解决方法: 加入 sudo 或可解决（有待验证）, gridSpacing 用 PlistBuddy 修改
+# 解决方法二: 桌面 ⌘J 点一下, 生成后再用 PlistBuddy
+# 方法二的缺点: 操作更麻烦, 不予讨论
+sudo osascript << EOF
+tell application "Finder"
+	set iconView to icon view options of window of desktop
+	tell iconView
+		set arrangement to snap to grid
+		set icon size to 52
+		-- Cant set gridSpacing
+		set text size to 10
+		set label position to bottom
+		set shows icon preview to true
+		set shows item info to true
+	end tell
+end tell
+EOF
+# 修改 Desktop 间距
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 1" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+
+# 修改所有 column 视图的标准样式
+# 存在问题: 默认情况下 com.apple.finder 中没有 ColumnView
+# 解决方法: 用 AppleScript 操作
+sudo osascript << EOF
+tell application "Finder"
+	make new Finder window
+	activate
+	set theWindow to the front window
+	set theView to column view options of theWindow
+	set text size of theView to 15
+	close every window
+end tell
+EOF
+
+# 修改所有 Icon 视图的标准样式
+# 存在问题: defaults 命令无法修改子项
+# 解决方法: 用 /usr/libexec/PlistBuddy
+# 方法的缺点: 无法操作不存在的文件、属性, 但是 AppleScript 处理后必定生成 com.apple.finder.plist, 故已被解决
+# 根据名称排序
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy name" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 图标尺寸
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 64" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 图标间距
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 42" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 名称大小
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:textSize 12" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 显示 label
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:labelOnBottom 1" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 显示图标预览
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showIconPreview 1" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+# 显示 info
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo 1" /Users/$USERNAME/Library/Preferences/com.apple.finder.plist
+
+# 设置 column 为默认视图
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: icnv, glyv, nlsv
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+# 设置 name 为默认分组
+defaults write com.apple.finder FXPreferredGroupBy -string 'Name'
+
+# ---------------------- Finder - Preference -------------------------
+# 桌面显示项目
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+# “访达”新窗口打开位置
+#              : PfCm, PfHm, PfDe, PfDo
+# Volume       : PfVo
+# All My Files : PfAF
+# Other…       : PfLo
+# ... NewWindowTargetPath "file://..."
+defaults write com.apple.finder NewWindowTarget -string 'PfHm'
+# 总是新标签打开
+defaults write com.apple.finder FinderSpawnTab 1
+# 清空标签
+defaults write com.apple.finder FavoriteTagNames '()'
+# 显示所有文件扩展名
+defaults write -g AppleShowAllExtensions 1
+# 修改扩展名不提示
+defaults write com.apple.finder FXEnableExtensionChangeWarning 0
+# 文件夹显示在前面
+defaults write com.apple.finder "_FXSortFoldersFirst" 1
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" 1
+# 显示隐藏文件
+# defaults write com.apple.finder AppleShowAllFiles 1
+# 搜索位置 当前文件夹
+defaults write com.apple.finder FXDefaultSearchScope 'SCcf'
+# 不隐藏 Library
+chflags nohidden ~/Library
+# 各种 Bar
+defaults write com.apple.finder ShowPathbar 1
+defaults write com.apple.finder ShowStatusBar 0
+defaults write com.apple.finder ShowSidebar 1
+defaults write com.apple.finder SidebarWidth 194
+defaults write com.apple.finder ShowPreviewPane 0
+defaults write com.apple.finder ShowRecentTags 0
+# 工具栏仅图标
+defaults write com.apple.finder "NSToolbar Configuration Browser" -dict-add "TB Display Mode" 2
+defaults write com.apple.finder "NSToolbar Configuration Browser" -dict-add "TB Item Identifiers" '("com.apple.finder.BACK","com.apple.finder.SRCH")'
+# 禁止 下载警告 Turn off the “Application Downloaded from Internet” quarantine warning.
+defaults write com.apple.LaunchServices "LSQuarantine" -bool false
+# 禁止 在 USB 介质上创建 DS_Store
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+# 禁止 在云端创建 DS_Store
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# 可选 Allow quitting Finder via ⌘ + Q , doing so will also hide desktop icons
+# defaults write com.apple.finder QuitMenuItem -bool true
+
+# ------------------------- Safari --------------------------
+# 首页, 展示背景图片和收藏, 其他全关闭
+defaults write com.apple.Safari ShowBackgroundImageInFavorites 1
+defaults write com.apple.Safari ShowFavorites 0
+defaults write com.apple.Safari ShowFrequentlyVisitedSites 0
+defaults write com.apple.Safari ShowHighlightsInFavorites 0
+defaults write com.apple.Safari ShowPrivacyReportInFavorites 0
+defaults write com.apple.Safari ShowReadingListInFavorites 0
+defaults write com.apple.Safari HideStartPageSiriSuggestionsEmptyItemView 0
+defaults write com.apple.Safari ShowSiriSuggestionsPreference 0
+# 默认下载地址
+defaults write com.apple.Safari NSNavLastRootDirectory "~/Desktop"
+# 下载成功移除
+defaults write com.apple.Safari DownloadsClearingPolicy 2
+# 关闭安全打开
+defaults write com.apple.Safari AutoOpenSafeDownloads 0
+# 窗口最大化
+defaults write com.apple.Safari "NSWindow Frame BrowserWindowFrame" "0 0 1440 900 0 0 1440 900"
+# 紧凑标签, 好看
+defaults write com.apple.Safari ShowStandaloneTabBar 0
+# 在标签页中始终显示网站标题
+defaults write com.apple.Safari EnableNarrowTabs 0
+# 始终在新标签页打开
+defaults write com.apple.Safari TabCreationPolicy 2
+# 打开新标签页或窗口后，使其成为活跃标签页或窗口
+defaults write com.apple.Safari OpenNewTabsInFront 1
+# 关闭自动填充
+defaults write com.apple.Safari AutoFillCreditCardData 0
+defaults write com.apple.Safari AutoFillFromAddressBook 0
+defaults write com.apple.Safari AutoFillMiscellaneousForms 0
+defaults write com.apple.Safari AutoFillPasswords 0
+# 设置搜索引擎
+defaults write com.apple.Safari SearchProviderShortName Bing
+# 关闭快速网站搜索
+defaults write com.apple.Safari WebsiteSpecificSearchEnabled 0
+# 关闭后台载入最佳结果
+defaults write com.apple.Safari PreloadTopHit 0
+# 启用开发者菜单
+defaults write com.apple.Safari IncludeDevelopMenu 1
+# 清空工具栏
+defaults write com.apple.Safari "NSToolbar Configuration BrowserToolbarIdentifier-v4.6" -dict-add "TB Item Identifiers" "(InputFieldsToolbarIdentifier,UnifiedTabBarToolbarIdentifier)"
---
+++ Necessary_config/sys_pre.sh
@@ -0,0 +1,292 @@
+# ----------------------- Dock ----------------------
+# Dock 大小
+# defaults write com.apple.dock tilesize -int 60
+defaults write com.apple.dock "tilesize" -int 16
+# 放大效果
+# magnification = 1;
+# 位置
+defaults write com.apple.dock "orientation" -string 'left'
+# 缩小效果
+defaults write com.apple.dock "mineffect" -string 'scale'
+# 关闭弹跳打开
+defaults write com.apple.dock "launchanim" -bool false
+# 自动隐藏
+defaults write com.apple.dock "autohide" -bool true
+# 自动隐藏延迟
+defaults write com.apple.dock "autohide-delay" -float 0
+# 打开应用显示指示灯
+defaults write com.apple.dock "show-process-indicators" 1
+# 更简单的方法（未测试）, empty dock and only show opened app
+# defaults write com.apple.dock "static-only" -bool "true" && killall Dock
+# 不显示最近使用
+defaults write com.apple.dock "show-recents" -bool false
+# Dock 栏固定的应用
+defaults write com.apple.dock "persistent-apps" -string '{}'
+# 垃圾桶边上的应用
+defaults write com.apple.dock "persistent-others" -string '{}'
+
+# ------------------------ Menu Bar -------------------------
+# 24-隐藏 18-总是显示 2-激活时显示
+# 显示 WiFi
+defaults -currentHost write com.apple.controlcenter WiFi -int 24
+# 隐藏 蓝牙
+defaults -currentHost write com.apple.controlcenter Bluetooth -int 24
+# 隐藏 AirDrop
+defaults -currentHost write com.apple.controlcenter AirDrop -int 24
+# 隐藏 专注模式
+defaults -currentHost write com.apple.controlcenter FocusModes -int 24
+# 隐藏 键盘亮度
+defaults -currentHost write com.apple.controlcenter KeyboardBrightness -int 24
+# 隐藏 屏幕镜像
+defaults -currentHost write com.apple.controlcenter ScreenMirroring -int 24
+# defaults write com.apple.airplay "showInMenuBarIfPresent" -bool false
+# 隐藏 显示器
+defaults -currentHost write com.apple.controlcenter Display -int 24
+# 隐藏 声音
+defaults -currentHost write com.apple.controlcenter Sound -int 24
+# 隐藏 正在播放
+defaults -currentHost write com.apple.controlcenter NowPlaying -int 24
+# 隐藏 辅助功能
+defaults -currentHost write com.apple.controlcenter AccessibilityShortcuts -int 24
+# 显示 电池
+defaults -currentHost write com.apple.controlcenter Battery -int 18
+defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -int 24
+# 隐藏 用户切换
+defaults -currentHost write com.apple.controlcenter UserSwitcher -int 24
+# 隐藏 Spotlight
+defaults -currentHost write com.apple.Spotlight MenuItemHidden -int 0
+# 隐藏 Siri
+defaults write com.apple.Siri "StatusMenuVisible" -bool false
+# 设置时钟
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE d MMM HH:mm\""
+
+# ------------------- Trackpad - Point & Click ------------------
+# 触控板灵敏度
+defaults write -g "com.apple.trackpad.scaling" -int 2
+# 禁用强力点击
+defaults write com.apple.AppleMultitouchTrackpad ForceSuppressed -bool true
+# 启用三指查询
+defaults -currentHost write -g "com.apple.trackpad.threeFingerTapGesture" -int 2
+
+# ------------------- Trackpad - Scroll & Zoom -------------------
+# 启用自然滚动
+defaults write -g "com.apple.swipescrolldirection" -bool true
+# 启用双指放大
+defaults -currentHost write -g "com.apple.trackpad.pinchGesture" -bool true
+# 禁用双击缩放
+defaults -currentHost write -g "com.apple.trackpad.twoFingerDoubleTapGesture" -bool false
+# 启用双指旋转
+defaults -currentHost write -g "com.apple.trackpad.rotateGesture" -bool true
+
+# ------------------- Trackpad - More gestures -------------------
+# 取消 双指 前进返回
+defaults write -g AppleEnableSwipeNavigateWithScrolls -bool false
+# 取消 双指 通知中心
+defaults -currentHost write -g "com.apple.trackpad.twoFingerFromRightEdgeSwipeGesture" -int 0
+# 启用 三指 拖移
+defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool true
+# 启用 control command 任意位置拖移
+defaults write -g NSWindowShouldDragOnGesture -bool true
+# 启用 四指 左右桌面
+defaults -currentHost write -g "com.apple.trackpad.fourFingerHorizSwipeGesture" -int 2
+# 启用 四指 向上调度
+defaults write com.apple.dock showMissionControlGestureEnabled -bool true
+# 禁用 四指 向下
+defaults write com.apple.dock showAppExposeGestureEnabled -bool false
+# 禁用 捏合控制台, 张开桌面
+defaults -currentHost write -g "com.apple.trackpad.fiveFingerPinchSwipeGesture" -int 0
+defaults write com.apple.dock showLaunchpadGestureEnabled -bool false
+defaults write com.apple.dock showDesktopGestureEnabled -bool false
+
+# ------------------- Keyboard - Keyboard -------------------
+# 禁用持续按住触发西班牙语
+defaults write -g ApplePressAndHoldEnabled -bool false
+# 重复频率, 存在的问题 见下
+# 1. seems to conflict with karabiner
+# 2. 不会显示在设置中
+# 3. 需要退出登录生效
+defaults write -g KeyRepeat -int 1
+# 重复前的 delay, 问题同上
+defaults write -g InitialKeyRepeat -int 13
+# 直接功能键
+defaults write -g com.apple.keyboard.fnState -bool false
+
+# ------------------- Keyboard - Text -------------------
+# 取消更正
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# 取消首字母大写
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# 取消自动逗号
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# ------------------- Keyboard - Shortcuts -------------------
+## Remove System shortcuts dont use
+### ------------------------- ###
+### 移除 显示/隐藏 Dock（52, 64、65未知）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 52 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 mission control（32、34）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 32 "<dict><key>enabled</key><false/></dict>"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 34 "<dict><key>enabled</key><false/></dict>"
+### 移除 Application window（33、35）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 33 "<dict><key>enabled</key><false/></dict>"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 35 "<dict><key>enabled</key><false/></dict>"
+### 移除 show desktop（36、37）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 36 "<dict><key>enabled</key><false/></dict>"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 37 "<dict><key>enabled</key><false/></dict>"
+### 移除 switch to desktop1（118）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 118 "<dict><key>enabled</key><false/></dict>"
+### 移除 switch to desktop2（119）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 119 "<dict><key>enabled</key><false/></dict>"
+### 移除 switch to desktop3（120）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 120 "<dict><key>enabled</key><false/></dict>"
+### 移除 quick note（190）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 190 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 open keyboard access（12）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 12 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F2（7）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 7 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F3（8）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 8 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F4（9）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 9 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F5（10）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 10 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F6（11）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 11 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F7（13）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 13 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^F8（57）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 57 "<dict><key>enabled</key><false/></dict>"
+### 移除 cmd丶（27）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 27 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 前一个输入法（60）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "<dict><key>enabled</key><false/></dict>"
+### 移除 后一个输入法（61）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 cmdshift3 截图（28）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 28 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^cmdshift3 截图（29）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 29 "<dict><key>enabled</key><false/></dict>"
+### 移除 cmdshift4 截图（30）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 30 "<dict><key>enabled</key><false/></dict>"
+### 移除 ^cmdshift4 截图（31）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 31 "<dict><key>enabled</key><false/></dict>"
+### 移除 cmdshift5 截图（184）
+# defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 184 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 spotlight cmd space（64）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "<dict><key>enabled</key><false/></dict>"
+### 移除 option cmd space（65）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 移除 增强、减弱对比度（25、26）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 25 "<dict><key>enabled</key><false/></dict>"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 26 "<dict><key>enabled</key><false/></dict>"
+### 移除 反转颜色（21）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 21 "<dict><key>enabled</key><false/></dict>"
+### 移除 voice over（59）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 59 "<dict><key>enabled</key><false/></dict>"
+### 移除 accessibliy control（162）
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 162 "<dict><key>enabled</key><false/></dict>"
+### ------------------------- ###
+### 显示帮助（98）
+
+# ------------------- Keyboard - Input Sources -------------------
+# Seen Remove ABC Input Method
+
+# ------------- Custom System shortcuts ------------------
+### ⌘ caps（⌃）3 全功能截图
+defaults write com.apple.symbolichotkeys.plist AppleSymbolicHotKeys -dict-add 184 "
+  <dict>
+    <key>enabled</key><true/>
+    <key>value</key><dict>
+      <key>parameters</key>
+      <array>
+        <integer>51</integer>
+        <integer>20</integer>
+        <integer>1310720</integer>
+      </array>
+      <key>type</key><string>standard</string>
+    </dict>
+  </dict>
+"
+### ⌘ caps（⌃）J move to left desktop（79, 80未知）
+defaults write com.apple.symbolichotkeys.plist AppleSymbolicHotKeys -dict-add 79 "
+  <dict>
+    <key>enabled</key><true/>
+    <key>value</key><dict>
+      <key>parameters</key>
+      <array>
+        <integer>106</integer>
+        <integer>38</integer>
+        <integer>1310720</integer>
+      </array>
+      <key>type</key><string>standard</string>
+    </dict>
+  </dict>
+"
+### ⌘ caps（⌃）K move to right desktop（81, 82未知）
+defaults write com.apple.symbolichotkeys.plist AppleSymbolicHotKeys -dict-add 81 "
+  <dict>
+    <key>enabled</key><true/>
+    <key>value</key><dict>
+      <key>parameters</key>
+      <array>
+        <integer>107</integer>
+        <integer>40</integer>
+        <integer>1310720</integer>
+      </array>
+      <key>type</key><string>standard</string>
+    </dict>
+  </dict>
+"
+# --------- Custom Application Shortcuts - English Version ----------
+# All - Preference ⌘丶
+defaults write -g NSUserKeyEquivalents '{"Preferences..." = "@`";}'
+
+# Apple Application tab move
+# Cant work if change com.apple.safari NSUserKeyEquivalents
+defaults write -g NSUserKeyEquivalents -dict-add "\033Window\033Show Previous Tab" "@j"
+defaults write -g NSUserKeyEquivalents -dict-add "\033Window\033Show Next Tab" "@k"
+defaults write -g NSUserKeyEquivalents -dict-add "\033Window\033Pin Tab" "@p"
+
+# Chrome tab move
+# Cant work if change com.google.chrome NSUserKeyEquivalents
+defaults write -g NSUserKeyEquivalents -dict-add "\033Tab\033Select Previous Tab" "@j"
+defaults write -g NSUserKeyEquivalents -dict-add "\033Tab\033Select Next Tab" "@k"
+defaults write -g NSUserKeyEquivalents -dict-add "\033Tab\033Pin Tab" "@p"
+
+# Finder
+defaults write -g -dict-add "\033View\033Clean Up" "@r"
+
+# 重置键盘、触控板
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
+# ---------------- 调度中心 ---------------
+# 取消 自动排列空间
+defaults write com.apple.dock "mru-spaces" -bool false
+# 开启 切换应用时切换空间
+defaults write NSGlobalDomain "AppleSpacesSwitchOnActivate" -bool true
+# 开启 应用分组
+defaults write com.apple.dock "expose-group-apps" -bool true
+# 关闭 显示器具有单独桌面
+defaults write com.apple.spaces "spans-displays" -bool true
+# 快捷键在键盘中已经取消
+
+# ---------------- 通用 ------------------
+# 点击跳转
+defaults write NSGlobalDomain AppleScrollerPagingBehavior -int 1
+
+# ----------------- 声音 -----------------
+# 提醒音量
+defaults write NSGlobalDomain "com.apple.sound.beep.volume" -int 0
+
+# ----------------- 截图 -----------------
+# 禁用 截图阴影 Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+# 默认 jpg 格式
+defaults write com.apple.screencapture type JPG
